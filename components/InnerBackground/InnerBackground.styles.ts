a9549a568c19b1799a11d13e719ddfb395e74a62
muffin9 <jinlog9@gmail.com>
2022-10-23 08:59:51 +0900

    🛠 초기 Background, 필요 이미지 파일 추가, 컬러 색상 추가.

---
+++ components/InnerBackground/InnerBackground.styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const InnerBackground = styled.section`
+  width: 50.5rem;
+  height: 34rem;
+  background: url('/InnerBackground.png');
+  background-size: cover;
+`;
--- styles/theme.ts
+++ styles/theme.ts
@@ -2,7 +2,17 @@ import { Theme } from '@emotion/react';
 
 const calcRem = (size: number): string => `${size / 16}rem`;
 
-const colors = {};
+const colors = {
+  blue: '#55B2D4',
+  yellow: '#FFC900',
+  pink: '#EE3E61',
+  red: '#FF0000',
+  darkGray: '#666666',
+  gray: '#999999',
+  lightGray: '#D7DEE2',
+  sliver: '#DDDDDD',
+  lightSliver: '#D9D9D9',
+};
 
 const fontSize = {
   xs: calcRem(12),
