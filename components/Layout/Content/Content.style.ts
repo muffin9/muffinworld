5d6efa1fb1340d0d2cb215cd718749a1c7cd64ce
Muffin <jinlog9@gmail.com>
2022-11-02 00:05:45 +0900

    FE-content layout 약간의 수정 merge to FE
    * 🚧 FE: Contennt Layout 컴포넌트 작업 진행중 Tab 컴포넌트 배치
    
    * 🛠 FE: Content css 패딩값 조절 및 emotion css 속성 사용하도록 셋팅 조정

--- components/Layout/Content/Content.style.ts
+++ components/Layout/Content/Content.style.ts
@@ -1,13 +1,32 @@
 import styled from '@emotion/styled';
 
+import { flexbox } from '@/styles/mixin';
 import theme from '@/styles/theme';
+
+export const Tab = styled.div`
+  width: 57px;
+  height: 34px;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  margin-bottom: 10px;
+  ${flexbox({ jc: 'center', ai: 'center' })}
+  background-color: ${theme.colors.blue};
+  border: 1px solid ${theme.colors.black};
+`;
+
+export const TabContainer = styled.nav`
+  position: absolute;
+  top: 1rem;
+  right: -3.6rem;
+`;
 
 export const ContentLayout = styled.section`
   width: 32.25rem;
   height: 27.1875rem;
-  padding: 2rem;
+  padding: 1rem 1.5rem;
   margin-left: 3px;
-  border: 1px solid ${theme.colors.black};
   border-radius: 10px;
+  position: relative;
+  border: 1px solid ${theme.colors.black};
   background-color: ${theme.colors.white};
 `;
--- components/Layout/Content/index.tsx
+++ components/Layout/Content/index.tsx
@@ -1,13 +1,36 @@
+import Link from 'next/link';
 import React from 'react';
 
 import * as S from './Content.style';
+
+import Text from '@/components/common/Text';
+import theme from '@/styles/theme';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const ContentLayout = ({ children }: LayoutProps) => (
-  <S.ContentLayout>{children}</S.ContentLayout>
+  <S.ContentLayout>
+    {children}
+    <S.TabContainer>
+      <Link href="/">
+        <S.Tab>
+          <Text text="홈" size="small" color={theme.colors.white} />
+        </S.Tab>
+      </Link>
+      <Link href="game">
+        <S.Tab>
+          <Text text="게임" size="small" color={theme.colors.white} />
+        </S.Tab>
+      </Link>
+      <Link href="diary">
+        <S.Tab>
+          <Text text="다이어리" size="small" color={theme.colors.white} />
+        </S.Tab>
+      </Link>
+    </S.TabContainer>
+  </S.ContentLayout>
 );
 
 export default ContentLayout;
